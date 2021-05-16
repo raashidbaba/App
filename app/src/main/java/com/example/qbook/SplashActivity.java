f2c12a4018587c00c65b5e2422e79bd35d7f4e3e
raashidbaba <raashidbaba849@gmail.com>
2021-05-16 20:36:22 +0530

    Added Comment

--- app/src/main/java/com/example/qbook/SplashActivity.java
+++ app/src/main/java/com/example/qbook/SplashActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+/*import android.widget.TextView;*/
 public class SplashActivity extends AppCompatActivity {
        private TextView appName;
 
